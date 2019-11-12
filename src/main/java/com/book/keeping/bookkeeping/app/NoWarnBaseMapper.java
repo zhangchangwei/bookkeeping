ad4ce349a4282fe859a5c978be8d0eb75212c829
breamer <237693178@qq.com>
2019-11-12 21:09:02 +0800

    更新

---
+++ src/main/java/com/book/keeping/bookkeeping/app/NoWarnBaseMapper.java
@@ -0,0 +1,12 @@
+package com.book.keeping.bookkeeping.app;
+
+/**
+ * 基础mapper
+ *
+ * @author zhang.penghao
+ * @version V1.0
+ * @date 2019/11/7
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface NoWarnBaseMapper {
+}
---
+++ src/main/java/com/book/keeping/bookkeeping/mapper/AdminUserMapper.java
@@ -0,0 +1,7 @@
+package com.book.keeping.bookkeeping.mapper;
+
+import com.book.keeping.bookkeeping.common.BaseMapper;
+import com.book.keeping.bookkeeping.entity.AdminUser;
+
+public interface AdminUserMapper extends BaseMapper<AdminUser> {
+}
